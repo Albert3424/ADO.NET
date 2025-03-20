974eac1f91e48d7bfdcb8a75397b9a265d0263e4
Hanzo Hasashi <Izukudeku09@outlook.com>
2025-03-20 23:59:53 +0500

    Insert in progress

---
+++ ADO.NET/Connector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ADO.NET
+{
+	static class Connector
+	{
+		const int PADDING = 30;
+		const string CONNECTION_STRING = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		static readonly SqlConnection connection;
+		static Connector()
+		{
+			connection = new SqlConnection(CONNECTION_STRING);
+		}
+
+		public static void SelectDirectors()
+		{
+			Select("*", "Directors");
+		}
+
+		public static void SelectMovies()
+		{
+			Connector.Select("title,release_date,FORMATMESSAGE(N'%s %s', first_name, last_name)", "Movies, Directors", "director = director_id");
+		}
+
+		public static void Select(string columns, string tables, string condition = null)
+		{
+			//string cmd = "SELECT title,release_date,FORMATMESSAGE(N'%s %s', first_name, last_name) FROM Movies, Directors WHERE director = director_id";
+			string cmd = $"SELECT {columns} FROM {tables}";
+			if (condition != null) cmd += $" WHERE {condition}";
+			cmd += ";";
+
+			SqlCommand command = new SqlCommand(cmd, connection);
+
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+
+			if (reader.HasRows)
+			{
+				Console.WriteLine("===================================================================================");
+				for (int i = 0; i < reader.FieldCount; i++)
+					Console.Write(reader.GetName(i).PadRight(PADDING));
+				Console.WriteLine();
+				Console.WriteLine("===================================================================================");
+				while (reader.Read())
+				{
+					//Console.WriteLine($"{reader[0].ToString().PadRight(5)}\t{reader[2].ToString().PadRight(15)}\t{reader[1].ToString().PadRight(15)}");
+					for (int i = 0; i < reader.FieldCount; i++)
+					{
+						Console.Write(reader[i].ToString().PadRight(PADDING));
+					}
+					Console.WriteLine();
+				}
+			}
+
+			reader.Close();
+			connection.Close();
+		}
+
+		public static void InsertDirector(string first_name, string last_name)
+		{
+			string cmd = $"INSERT Directors(first_name, last_name) VALUES ({first_name}, {last_name})";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			command.ExecuteNonQuery();
+			connection.Close();
+		}
+	}
+}
--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -12,6 +12,7 @@ namespace ADO.NET
 	{
 		static void Main(string[] args)
 		{
+#if INTRO
 			const int PADDING = 30;
 			const string CONNECTION_STRING = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 			Console.WriteLine(CONNECTION_STRING);
@@ -44,7 +45,13 @@ namespace ADO.NET
 
 			reader.Close();
 			connection.Close();
+#endif
 
+			//Connector.Select("*", "Directors");
+			//Connector.Select("title,release_date,FORMATMESSAGE(N'%s %s', first_name, last_name)", "Movies, Directors", "director = director_id");
+			Connector.InsertDirector("George", "Martin");
+			Connector.SelectDirectors();
+			Connector.SelectMovies();
 			Console.ReadLine();
 		}
 	}
